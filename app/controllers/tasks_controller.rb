ed40aabee68b59c3f734925f5da39c435f06cc0b
Alice Teilhard <alice.teilhard@gmail.com>
2022-11-15 16:40:01 +0000

    delete done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -31,6 +31,14 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+
+
 end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
 
-
+  delete "tasks/:id", to: "tasks#destroy", as: :delete_task
   get "tasks/:id/edit", to: "tasks#edit", as: :edit_task #editatask
   patch "tasks/:id", to: "tasks#update"
   get "tasks/new", to: "tasks#new"  #making a new task
